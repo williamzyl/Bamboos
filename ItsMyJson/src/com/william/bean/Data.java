43c6d51f4c2ba927cf5d24b9545e5cc6a79324b3
zhangyl <zhangyl@zeasn.com>
2017-08-15 14:17:04 +0800

    project

---
+++ ItsMyJson/src/com/william/bean/Data.java
@@ -0,0 +1,19 @@
+package com.william.bean;
+
+import java.util.List;
+import java.util.Map;
+
+public class Data {
+	private Map<String,List<Video>> data;
+
+	public Map<String, List<Video>> getData() {
+		return data;
+	}
+
+	public void setData(Map<String, List<Video>> data) {
+		this.data = data;
+	}
+	
+	
+
+}
